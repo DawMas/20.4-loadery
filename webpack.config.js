18999b06db275e0d9aaca143704a3e1defa468ce
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-07-23 20:11:59 +0200

    Problem with npm start

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+//webpack.config.js
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exlude: 'node_modules.js',
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
