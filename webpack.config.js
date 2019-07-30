558df2bdb8a5820fca3434e821dfc7655dff8202
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-07-30 20:21:09 +0200

    Add title component

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exlude: 'node_modules.js',
+                exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
